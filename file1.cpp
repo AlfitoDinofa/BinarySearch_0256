dca389c2db3a6c361f4c80c93dba82b9763f5b07
AlfitoDinofa <alfitodinofa2006@gmail.com>
2025-05-02 09:36:26 +0700

    membuat prosedur input

--- file1.cpp
+++ file1.cpp
@@ -5,4 +5,25 @@ int element[10];
 int nPanjang;
 int x;
 
+void input(){  // prosedur input
+    while (true){
+        cout << "masukkan banyakknya elemen pada array: ";
+        cin >> nPanjang;
+        if (nPanjang <= 20){
+            break;
+        }
+        else {
+            cout << "\nArray maksimal 20 elemen. \n";
+        }
+    }
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukkan elemen array" << endl;
+    cout << "=====================" << endl;
+
+    for (int i=0;i<nPanjang;i++){ 
+         cout << "Data ke-" << (i+1) << " = ";
+         cin >> element[i];
+    }
+}
 
